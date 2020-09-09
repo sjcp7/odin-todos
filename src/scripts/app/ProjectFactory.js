60a916812f2ab610a5ca2aacb49943958171c7a9
Samuel Pedro <redsamuel.cruz7@gmail.com>
2020-09-10 00:06:21 +0100

    Add factories

--- src/scripts/app/ProjectFactory.js
+++ src/scripts/app/ProjectFactory.js
@@ -1,1 +1,29 @@
+const Project = (id, name, todos) => {
+  const _id = id;
+  let _name = name;
+  const _todos = todos;
 
+  const getId = () => _id;
+  const getName = () => _name;
+  const getTodos = () => _todos;
+
+  const renameProject = (newName) => { _name = newName }
+
+  const addTodo = (todo) => {
+    _todos.push(todo);
+  }
+  const removeTodo = (todoId) => {
+    _todos.splice(todoId, 1);
+  }
+
+  return {
+    getId,
+    getName,
+    getTodos,
+    renameProject,
+    addTodo,
+    removeTodo
+  }
+}
+
+export { Project }
