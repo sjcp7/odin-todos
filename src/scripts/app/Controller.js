429147c0070df33d5e9fe9c4609b3d958638e2f5
Samuel Pedro <redsamuel.cruz7@gmail.com>
2020-10-19 23:05:07 +0100

    add default project and 'getProject' method

--- src/scripts/app/Controller.js
+++ src/scripts/app/Controller.js
@@ -1,25 +1,29 @@
 import { Project } from './ProjectFactory';
 import { Todo } from './TodoFactory';
 import { Storage } from '../repositories/localStorage';
-
+import { v4 as uuid } from 'uuid';
 
 const Controller = () => {
 
   const db = Storage();
+  const defaultTodo = Todo(uuid(), 'My Todo', 'This is my todo', '2020-10-19', 'high', 'unchecked');
+  const defaultProject = Project(uuid(), 'Default', [defaultTodo]);
+
+  if (db.getProjects().length == 0) db.addProject(defaultProject);
 
   const getProjects = () => db.getProjects();
+
+  const getProject = (id) => db.getProject(id);
 
   const newProject = (id, name, todos=[]) => Project(id, name, todos);
 
   const newTodo = (id, title, description, deadline, priority) => Todo(id, title, description, deadline, priority);
   
-  const saveProjects = (projects) => db.saveProjects(projects);
-
   return {
     getProjects,
+    getProject,
     newProject,
-    newTodo,
-    saveProjects,
+    newTodo
   }
 }
 
