a9a08712c60e87e2117d6ce17ee7801e8a7e3f5c
Samuel Pedro <redsamuel.cruz7@gmail.com>
2020-11-05 22:56:31 +0100

    remove methods because of json incompatibility

--- src/scripts/app/ProjectFactory.js
+++ src/scripts/app/ProjectFactory.js
@@ -1,17 +1,8 @@
 const Project = (id, name, todos=[]) => {
-  const addTodo = (todo) => {
-    todos.push(todo);
-  }
-  const removeTodo = (todoId) => {
-    todos.splice(todoId, 1);
-  }
-
   return {
     id,
     name,
-    todos,
-    addTodo,
-    removeTodo
+    todos
   }
 }
 
