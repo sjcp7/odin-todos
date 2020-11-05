f3e7ba287f724d325cbe440afcfa4202387b64f2
Samuel Pedro <redsamuel.cruz7@gmail.com>
2020-11-05 22:59:33 +0100

    refactor code, fix bug and add status check

--- src/scripts/dom/task.js
+++ src/scripts/dom/task.js
@@ -1,10 +1,14 @@
 const taskCard = (task) => {
   const card = document.createElement('div');
   card.classList.add('task-card');
+  card.setAttribute('data-id', task.id);
 
   const section = document.createElement('section');
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
+  if (task.status == 'checked') {
+    checkbox.checked = true;
+  }
   const title = document.createElement('h2');
   title.textContent = task.title;
 
@@ -23,11 +27,13 @@ const taskCard = (task) => {
   const editBtn = document.createElement('i');
   editBtn.className = 'fas fa-edit fas-vert-align';
   const editBtnWrapper = document.createElement('span');
+  editBtnWrapper.classList.add('edit-todo-btn');
   editBtnWrapper.appendChild(editBtn);
 
   const delBtn = document.createElement('i');
   delBtn.className = 'fas fa-trash-alt fas-vert-align';
   const delBtnWrapper = document.createElement('span');
+  delBtnWrapper.classList.add('delete-todo-btn');
   delBtnWrapper.appendChild(delBtn);
 
   btns.appendChild(editBtnWrapper);
@@ -38,14 +44,8 @@ const taskCard = (task) => {
   card.appendChild(deadline);
   card.appendChild(priority);
   card.appendChild(btns);
-  const render = () => {
-    const containerCont = document.querySelector('#tasks-container');
-    containerCont.appendChild(card);
-  }
-  return {
-    card,
-    render
-  };
+
+  return card;
 }
 
 export { taskCard }
