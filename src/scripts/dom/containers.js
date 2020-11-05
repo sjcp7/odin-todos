cc868173c9b9f9fce3f43654260a2d5ddfb9165e
Samuel Pedro <redsamuel.cruz7@gmail.com>
2020-11-05 22:56:58 +0100

    refactor code

--- src/scripts/dom/containers.js
+++ src/scripts/dom/containers.js
@@ -1,31 +1,13 @@
-import { tasksBanner } from "./tasksBanner";
-
 const mainContainer = () => {
   const mainCont = document.createElement('main');
-  const render = () => {
-    const contentCont = document.querySelector('#content');
-    contentCont.appendChild(mainCont);
-  }
-  return {
-    mainCont,
-    render
-  }
+  return mainCont;
 }
 
 const tasksContainer = () => {
   const cont = document.createElement('div');
-  const banner = tasksBanner();
   cont.id = 'tasks-container';
   cont.classList.add('centered');
-  const render = () => {
-    const mainCont = document.querySelector('main');
-    mainCont.appendChild(cont);
-    banner.render();
-  }
-  return {
-    cont,
-    render
-  }
+  return cont;
 }
 
 export {
